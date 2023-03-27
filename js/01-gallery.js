aadd526d6434df8ec2ce6cd037516cb71d842056
Mykhaylo Yaremkevych <114567561+Mykhaylo33@users.noreply.github.com>
2023-03-27 22:57:05 +0300

    finished hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+  )
+  .join("");
+
+galleryEl.innerHTML = markupGallery;
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const lightModalEl = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+  lightModalEl.show();
+
+  galleryEl.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      lightModalEl.close();
+    }
+  });
+}
+
+galleryEl.addEventListener("click", onImageClick);
